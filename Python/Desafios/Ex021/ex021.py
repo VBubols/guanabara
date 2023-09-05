6025e9757cca9af58d6f7aca44a8773a1d7bb495
VBubols <vitorbubols@gmail.com>
2023-09-05 20:03:08 -0300

    Alterações

--- Python/Desafios/Ex021/ex021.py
+++ Python/Desafios/Ex021/ex021.py
@@ -1,4 +1,4 @@
 #Faça um programa em python que abra e reproduza o áudio de um arquivo MP3
-from pydub import AudioSegment 
-audio = AudioSegment.from_mp3("C:\Users\vitor\Git\guanabara\Python\Desafios\Ex021\Spider-man 2099 sound effect.mp3", format="mp3")
+import pydub
+audio = pydub.AudioSegment.from_mp3("C:\\Users\\vitor\\Downloads\\spiderhomem.mp3", format="mp3")
 audio.export("saida.mp3", format="mp3")
