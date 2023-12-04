d4e1a1608e75021c5720eb2d0e981d4b73cca0a8
VBubols <vitorbubols@gmail.com>
2023-12-04 20:48:13 -0300

    [EX21] wip

--- Python/Desafios/Ex021/ex021.py
+++ Python/Desafios/Ex021/ex021.py
@@ -1,4 +1,7 @@
 #Faça um programa em python que abra e reproduza o áudio de um arquivo MP3
-import pydub
-audio = pydub.AudioSegment.from_mp3("C:\\Users\\vitor\\Downloads\\spiderhomem.mp3", format="mp3")
-audio.export("saida.mp3", format="mp3")
+import pygame
+pygame.init()
+pygame.mixer.music.load(spiderman.mp3)
+pygame.mixer.music.play()
+pygame.time.delay(10000)
+pygame.quit()
